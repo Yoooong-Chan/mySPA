871171cc3fd725baa9c9343c1b2325b889e43acf
Yoooong-Chan <rhdlwmal1234@naver.com>
2020-06-22 11:37:53 +0900

    Javascript : nav__scrollIntoView

--- main.js
+++ main.js
@@ -13,3 +13,16 @@ document.addEventListener('scroll', () => {
     navbar.classList.remove('navbar--dark');
   }
 });
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+  const target = event.target;
+  const link = target.dataset.link; //target에있는 데이터셋에있는 링크
+  if (link == null) {
+    return;
+  }
+  console.log(event.target.dataset.link); //dataset안에 -가 link 이기때문에 -link붙임 dataset안에 우리가정의한 변수가 할당됨
+  const elmnt = document.querySelector(link);
+  elmnt.scrollIntoView({behavior: 'smooth'});
+});
